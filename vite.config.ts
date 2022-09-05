3e8c9c1ae15097e898398de6842f6213a03a0070
Saturday <zhangkairong123@qq.com>
2022-09-05 14:39:40 +0800

    chapter01: 了解从0搭建

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, 'src'), // 手动指定根目录位置
+  // root: path.join(__dirname, 'src'), // 手动指定根目录位置
   plugins: [react()],
 })
