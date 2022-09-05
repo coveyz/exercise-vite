84e318abcbc5d2bc33daf00bc9872bf96b566677
Saturday <zhangkairong123@qq.com>
2022-09-05 14:07:26 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: path.join(__dirname, 'src'), // 手动指定根目录位置
+  plugins: [react()],
+})
